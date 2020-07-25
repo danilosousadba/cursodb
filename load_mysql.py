fc2781949f37a0f03e946f6a4425ed7d4b4fcdfe
root <danilosousadba@gmail.com>
2020-07-25 14:17:04 -0300

    Add Python

--- load_mysql.py
+++ load_mysql.py
@@ -4,6 +4,10 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 from pandas.io import sql
+import sqlalchemy
+
+
+
 
 database_username = 'root'
 database_password = 'aluno'
@@ -14,38 +18,23 @@ database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{
                                                       database_ip, database_name))
 
 
-
-
-##frame.to_sql(con=database_connection, name={}, if_exists='replace'.format(tableName))
-
-
-
 mypath='dados/'
 
 for tablename in listdir(mypath):
     print(tablename)
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-""""
-tableName   = "UserVitals"
-dataFrame   = pd.DataFrame(data=userVitals)
-sqlEngine       = create_engine('mysql+pymysql://root:@127.0.0.1/test', pool_recycle=3600)
-dbConnection    = sqlEngine.connect()
-try:
+    print(tablename.split('.')[0])
+    tableName   = tablename
+    dataFrame   = pd.read_csv('{}{}'.format(mypath,tablename))
+    dbConnection    = database_connection.connect()
 
-    frame           = dataFrame.to_sql(tableName, dbConnection, if_exists='fail');
+    try:
+        dataFrame.to_sql(tableName, dbConnection, if_exists='fail');
 
-except ValueError as vx:
-
-    print(vx)
-
-except Exception as ex:
-
-    print(ex)
-
-else:
-
-    print("Table %s created successfully."%tableName);
-
-finally:
-
-    dbConnection.close() """
+    except ValueError as vx:
+        print(vx)
+    except Exception as ex:
+        print(ex)
+    else:
+        print("Table %s created successfully."%tableName);
+    finally:
+        dbConnection.close()

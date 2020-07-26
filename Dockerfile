15c347dd416262804fdfa602c0d4ba457c5417d3
root <danilosousadba@gmail.com>
2020-07-26 09:03:16 -0300

    Add Load Database

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,16 @@
-FROM mysql as db
+FROM ubuntu
 
-COPY load_mysql.py .
+COPY dados/ /dados/
+
+RUN apt-get update && apt-get install -y \
+    python3 python3-pip
 
 COPY requirements.txt .
 
-COPY dados/ /dados/
-
 COPY wait-for-it.sh .
 
-RUN apt-get update && apt-get install -y \
-    python3 python3-pip
-
-RUN python3 -m pip install -r requirements.txt
-
-FROM ubuntu as postdb
-
 COPY dados/ /dados/
 
-RUN apt-get update && apt-get install -y \
-    python3 python3-pip
+COPY load_mysql.py .
 
 RUN python3 -m pip install -r requirements.txt
--- load_mysql.py
+++ load_mysql.py
@@ -11,7 +11,7 @@ import sqlalchemy
 
 database_username = 'root'
 database_password = 'aluno'
-database_ip       = 'localhost'
+database_ip       = 'db'
 database_name     = 'curso'
 database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
                                                format(database_username, database_password,
@@ -21,14 +21,13 @@ database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{
 mypath='dados/'
 
 for tablename in listdir(mypath):
-    print(tablename)
     print(tablename.split('.')[0])
     tableName   = tablename
     dataFrame   = pd.read_csv(mypath+tablename,index_col=False)
     dbConnection    = database_connection.connect()
 
     try:
-        dataFrame.to_sql(tableName, dbConnection, if_exists='replace');
+        dataFrame.to_sql(tablename.split('.')[0], dbConnection, if_exists='replace');
 
     except ValueError as vx:
         print(vx)
